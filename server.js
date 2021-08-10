7718ae25fa406dca2b8b3fe98289634376ec9ecb
jingfei <987003803@qq.com>
2021-08-10 17:27:54 +0800

    完成目标一

--- server.js
+++ server.js
@@ -21,7 +21,28 @@ var server = http.createServer(function(request, response){
 
   console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
 
-  response.statusCode = 200
+ if(path==='/register'&&method==='POST'){//方法必须是 post 
+   response.setHeader('Content-Type', 'text/html;charset=utf-8')
+   const userArray = JSON.parse(fs.readFileSync("./db/users.json"))
+   const array = [] //获取数据时需要一点一点的获取
+   request.on('data', (chunk) => {//data 上传事件 chunk 一小段数据
+     array.push(chunk)
+   })
+   request.on('end', () => {
+     const string = Buffer.concat(array).toString()//把乱码组合到一起，变成想要的字符串
+     const obj = JSON.parse(string)
+     const lastUser = userArray[userArray.length - 1]
+     const newUser = {
+       id:lastUser ? lastUser.id + 1 : 1 ,
+       name: obj.name,
+       password:obj.password
+     }
+     userArray.push(newUser)
+     fs.writeFileSync('./db/users.json',JSON.stringify(userArray))
+     response.end()
+   })
+   
+  }else{response.statusCode = 200
   // 默认首页
   const filePath = path === '/' ? '/index.html' : path
   const index = filePath.lastIndexOf('.')
@@ -45,7 +66,9 @@ var server = http.createServer(function(request, response){
   }
   response.write(content)
   response.end()
+}
 
+  
   /******** 代码结束，下面不要看 ************/
 })
 
