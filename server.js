3117126227cec28a639f37f268eb874f2b04fc3d
jingfei <987003803@qq.com>
2021-08-11 19:09:39 +0800

    运用到 sesssion 将登录页登陆后写上用户名

--- server.js
+++ server.js
@@ -18,10 +18,71 @@ var server = http.createServer(function(request, response){
   var method = request.method
 
   /******** 从这里开始看，上面不要看 ************/
-
+  const session = JSON.parse(fs.readFileSync('./session.json'))
   console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
-
- if(path==='/register'&&method==='POST'){//方法必须是 post 
+ 
+  if (path === '/sign_in' && method === 'POST') {
+   response.setHeader('Content-Type', 'text/html;charset=utf-8')
+   const userArray = JSON.parse(fs.readFileSync("./db/users.json"))
+   const array = [] //获取数据时需要一点一点的获取
+   request.on('data', (chunk) => {//data 上传事件 chunk 一小段数据
+     array.push(chunk)
+   })
+   request.on('end', () => {
+     const string = Buffer.concat(array).toString()//把乱码组合到一起，变成想要的字符串
+     const obj = JSON.parse(string)
+  
+     const user = userArray.find((user) => user.name === obj.name && user.password === obj.password)
+    
+     if (user !== undefined) {
+       response.statusCode = 200
+       const random = Math.random()//记录随机数
+       const session = JSON.parse(fs.readFileSync('./session.json'))
+       session[random] = { user_id: user.id }
+       fs.writeFileSync('./session.json',JSON.stringify(session))
+       response.setHeader('set-Cookie', `session_id=${random};HttpOnly`)//设置 cookie 并前端浏览器无法访问
+       response.end()
+     } else {
+       response.setHeader('Content-Type', 'text/json;charset=utf-8')
+       response.statusCode = 400
+       response.end(`{"errorCode":4001}`)
+     }
+   })
+  } else if(path==='/home.html'){
+   //在回应头上加 cookie 检查有没有
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    let cookie = request.headers["cookie"];//读取 cookie
+    let sessionId
+    
+    try {
+      sessionId = cookie.split(';').filter(s => s.indexOf('session_id=') >= 0)[0].split('=')[1]
+    //把 cookie 分开 然后选择元素里带有 user_id 的元素然后再分割选择数组里的第二项
+    } catch (error) {
+      //没有 user_id
+    }
+    console.log(sessionId)
+    const homeHtml = fs.readFileSync("./public/home.html").toString()
+    if (sessionId&&session[sessionId]) {
+      
+      const userId = session[sessionId].user_id
+      console.log(typeof userId)
+      
+      const userArray = JSON.parse(fs.readFileSync("./db/users.json"))
+      const user = userArray.find(user => user.id === userId)
+      
+      let string = ''
+      if (user) {
+        string = homeHtml.replace('{{loginStatus}}', '已登录')
+              .replace('{{user.name}}',user.name)
+      }
+      response.write(string)
+    } else {
+       string = homeHtml.replace('{{loginStatus}}', '未登录')
+        .replace('{{user.name}}', '')
+      response.write(string)
+    }
+    response.end()
+  }else if(path==='/register'&&method==='POST'){//方法必须是 post 
    response.setHeader('Content-Type', 'text/html;charset=utf-8')
    const userArray = JSON.parse(fs.readFileSync("./db/users.json"))
    const array = [] //获取数据时需要一点一点的获取
